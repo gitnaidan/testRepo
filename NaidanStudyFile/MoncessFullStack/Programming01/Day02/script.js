ed22673b13588051ae260bd17a718094ba6e4d5a
N41dan <Naidan.jigjiddorj@gmail.com>
2025-01-20 22:18:17 -0500

    Script

---
+++ NaidanStudyFile/MoncessFullStack/Programming01/Day02/script.js
@@ -0,0 +1,23 @@
+/! Variable - storage /
+
+// String - Hello World
+
+// Declaration and Initialization 
+
+var str =  "Hello World";
+console.log(str);
+
+
+//? Number 
+var myNum = 123;
+console.log(myNum)
+
+//?Boolean - true or false
+
+var isStudent = true
+
+
+//? Null
+
+var myName;
+console.log(myName)
